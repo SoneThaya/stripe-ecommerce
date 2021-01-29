d30a8239a220bb6c319b2b9529146f7a3293cde0
Sone <sthaya@gmail.com>
2021-01-28 19:15:18 -0800

    created custom shipping address component

--- stripe-ecommerce/src/components/checkout/Checkout.jsx
+++ stripe-ecommerce/src/components/checkout/Checkout.jsx
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import Layout from "../shared/Layout";
-import StripeCheckout from "./stripe-checkout/StripeCheckout";
+import ShippingAddress from "../custom-checkout/ShippingAddress";
 
 import "./checkout.styles.scss";
 
 const Checkout = () => {
   const { itemCount, total } = useContext(CartContext);
+  const [shipping, setShipping] = useState(null);
+  const addressShown = {
+    display: shipping ? "none" : "block",
+  };
 
   return (
     <Layout>
@@ -14,7 +18,9 @@ const Checkout = () => {
         <h2>Checkout Summary</h2>
         <h3>{`Total Items: ${itemCount}`}</h3>
         <h4>{`Amount to Pay: $${total}`}</h4>
-        <StripeCheckout />
+        <div style={addressShown}>
+          <ShippingAddress setShipping={setShipping} />
+        </div>
       </div>
     </Layout>
   );
