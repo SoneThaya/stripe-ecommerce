73bca83793bceb5ff281c974eebddcfde1467b66
Sone <sthaya@gmail.com>
2021-01-29 20:10:05 -0800

    created user context provider

--- stripe-ecommerce/src/index.js
+++ stripe-ecommerce/src/index.js
@@ -8,6 +8,7 @@ import ProductsContextProvider from "./context/ProductsContext";
 import CartContextProvider from "./context/CartContext";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import UserContextProvider from "./context/UserContext";
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
@@ -16,7 +17,9 @@ ReactDOM.render(
     <ProductsContextProvider>
       <CartContextProvider>
         <Elements stripe={stripePromise}>
-          <App />
+          <UserContextProvider>
+            <App />
+          </UserContextProvider>
         </Elements>
       </CartContextProvider>
     </ProductsContextProvider>
