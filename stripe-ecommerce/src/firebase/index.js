fb02bdb35cd76ad985736e25cc7bdc699ba7fc82
Sone <sthaya@gmail.com>
2021-01-29 19:58:02 -0800

    created user profile document

--- stripe-ecommerce/src/firebase/index.js
+++ stripe-ecommerce/src/firebase/index.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore"; // for the db
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyDNpGIHSUYexPMkHDzKUf9PPCA2J3ZBDA0",
@@ -13,5 +14,33 @@ const config = {
 firebase.initializeApp(config);
 
 const firestore = firebase.firestore();
+const auth = firebase.auth();
 
-export { firestore };
+const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error);
+    }
+  }
+
+  return userRef;
+};
+
+export { firestore, createUserProfileDocument, auth };
