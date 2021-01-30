7276cf8d70a8a2dd0621941f191a13206004592a
Sone <sthaya@gmail.com>
2021-01-29 20:38:11 -0800

    worked on header component to display sign up or sign out or sign in

--- stripe-ecommerce/src/components/header/Header.jsx
+++ stripe-ecommerce/src/components/header/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "../cart-icon/CartIcon";
+import { auth } from "../../firebase";
+import { UserContext } from "../../context/UserContext";
 import "./header.styles.scss";
 
 const Header = () => {
+  const { user } = useContext(UserContext);
+  console.log(user);
+
   return (
     <nav className="nav-menu container">
       <div className="logo">
@@ -16,6 +21,17 @@ const Header = () => {
         <li>
           <Link to="/shop">Shop</Link>
         </li>
+        {!user && (
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+        )}
+        {user && <li onClick={() => auth.signOut()}>Sign Out</li>}
+        {!user && (
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        )}
       </ul>
       <CartIcon />
     </nav>
--- stripe-ecommerce/src/components/sign-in/SignIn.jsx
+++ stripe-ecommerce/src/components/sign-in/SignIn.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../shared/Layout";
 import { Formik } from "formik";
+import { withRouter } from "react-router-dom";
+import { auth } from "../../firebase";
 import "../sign-up/signUp.styles.scss";
 
-const SignIn = () => {
+const SignIn = ({ history: { push } }) => {
+  const [error, setError] = useState(null);
+
   const initialValues = {
     email: "",
     password: "",
+  };
+
+  const handleSignIn = async (values, { setSubmitting }) => {
+    const { email, password } = values;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setSubmitting(false);
+      push("/shop");
+    } catch (error) {
+      console.log(error);
+      setSubmitting(false);
+      setError(error);
+    }
   };
 
   return (
     <Layout>
       <h1>Sign In</h1>
       <div className="form-container">
-        <Formik
-          initialValues={initialValues}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
+        <Formik initialValues={initialValues} onSubmit={handleSignIn}>
           {(values, handleChange, handleSubmit, isSubmitting) => {
             return (
               <form onSubmit={handleSubmit}>
@@ -51,6 +63,9 @@ const SignIn = () => {
                     Sign In
                   </button>
                 </div>
+                <div className="error-message">
+                  {error && <p>{error.message}</p>}
+                </div>
               </form>
             );
           }}
@@ -60,4 +75,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default withRouter(SignIn);
