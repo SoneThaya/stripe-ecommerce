9d3cf099f6ee5d7e4428cd13197a198fdebd3b76
Sone <sthaya@gmail.com>
2021-02-01 17:37:33 -0800

    created setup intent file funtion

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const createCheckoutSession = require("./api//checkout");
 const webhook = require("./api/webhook");
 const paymentIntent = require("./api/paymentIntent");
 const decodeJWT = require("./auth/decodeJWT");
+const setupIntent = require("./api/setupIntent");
+const validateUser = require("./auth/validateUser");
 
 const app = express();
 const port = 8080;
@@ -24,6 +26,8 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/create-checkout-session", createCheckoutSession);
 
 app.post("/create-payment-intent", paymentIntent);
+
+app.post("/save-payment-method", validateUser, setupIntent);
 
 app.post("/webhook", webhook);
 
